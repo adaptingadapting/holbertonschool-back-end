c2dc07b54f1d478b9c38cc8aac729749bc8a8269
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-05-29 20:13:41 -0300

    task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import csv
+from requests import get
+from sys import argv
+from json import dump
+
+if __name__ == "__main__":
+    e_id = argv[-1]
+
+    user_info = get(f"https://jsonplaceholder.typicode.com/users"
+                    ).json()
+
+    # e_name = user_info["name"]
+
+    tasks_in = get(f"https://jsonplaceholder.typicode.com/todos"
+                   ).json()
+
+    """
+    completed = 0
+    not_completed = 0
+    list_of_c_tasks = []
+
+    for task in tasks_in:
+        if task["completed"] is True:
+            completed += 1
+            list_of_c_tasks.append(task["title"])
+        else:
+            not_completed += 1
+    total_tasks = completed + not_completed
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(e_name, completed, total_tasks))
+
+    for element in list_of_c_tasks:
+        print(f"\t {element}")
+
+    # task 2
+
+    with open(f"{e_id}.csv", 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in tasks_in:
+            writer.writerow([e_id, user_info['username'],
+                             task['completed'], task['title']])
+    """
+
+    for employee in user_info:
+        new_dict = {employee["id"]: []}
+        for task in tasks_in:
+            new_dict2 = {}
+            new_dict2["username"] = employee["username"]
+            new_dict2["task"] = task["title"]
+            new_dict2["completed"] = task["completed"]
+
+            new_dict[employee["id"]].append(new_dict2)
+
+    with open("todo_all_employees.json", "w") as f:
+        dump(new_dict, f)
